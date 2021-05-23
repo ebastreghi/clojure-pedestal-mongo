e4c7ad05c2e217634966c7b6aee6e8b3a58a001f
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-05-23 14:27:13 -0300

    Initial project
    Initial project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-pedestal-mongo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_pedestal_mongo/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-pedestal-mongo.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-pedestal-mongo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
